b5ee9a772c15b5aaef9116272f9d9bd32634deac
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-12-24 11:06:38 +0500

    create a routing table

--- public/index.php
+++ public/index.php
@@ -19,3 +19,14 @@ require '../Core/Router.php';
 $router = new Router();
 
 echo get_class($router);
+
+//Add the rotues
+$router->add('/', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'create']);
+
+//Display the routing table
+
+echo '<pre>';
+var_dump($router->getRoutes());
+echo '<pre>';
