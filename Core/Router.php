d4fa78363630e8973b32eb12c08ec223612c1a47
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-12-24 11:03:58 +0500

    create add and get routes method on Router

--- Core/Router.php
+++ Core/Router.php
@@ -6,6 +6,31 @@
 
 class Router
 {
+    /**
+     * Associative array of routes (the routing table)
+     * @var array
+     */
+    protected $routes = [];
 
+    /**
+     * Add a route to the routing table
+     * 
+     * @param string $route The route URL
+     * @param array $params Parameters (controller,actions,etc.)
+     * 
+     * @return void
+     */
+    public function add($route, $params): void
+    {
+        $this->routes[$route] = $params;
+    }
+
+    /**
+     * Get all the routes from the routing table
+     * @return @array
+     */
+    public function getRoutes(): array
+    {
+        return $this->routes;
+    }
 }
-  
